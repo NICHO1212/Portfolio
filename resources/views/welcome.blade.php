52b9100058ff561fefff8449e3d509af9082f09e
Esteban el Dios <una.esteban@gmail.com>
2021-01-19 18:33:34 -0600

    welcome.3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
 
         <!-- Styles -->
-        @include(css.welcome)
+        @include('css')
 
     </head>
     <body>
